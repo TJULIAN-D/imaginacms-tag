243e23da0e9cba000fd64fb7e01b313ea43bed6e
Cristian Marin <marin101998@gmail.com>
2022-05-19 19:19:50 -0500

    +update | component with component button and add params to component button +delete | layout 2 component tag

--- Resources/views/frontend/components/tags/layouts/tags-layout-1/index.blade.php
+++ Resources/views/frontend/components/tags/layouts/tags-layout-1/index.blade.php
@@ -1,11 +1,22 @@
 <div class="tag">
   @if(!$item->tags->isEmpty())
     <br>
-    <p>{{trans('tag::tag.list')}}</p>
+    <p class="{{$titleClass}}">{{trans('tag::tag.list')}}</p>
     <span class="tags-links">
       @foreach($item->tags as $tag)
-        <a class="btn btn-primary m-1" href="{{route(locale(). '.tag.slug',['slug' => $tag->slug])}}"
-           rel="tag">{{$tag->name}}</a>
+        <x-isite::button buttonClasses="{{$buttonClasses}}"
+                         href="{{route(locale(). '.tag.slug',['slug' => $tag->slug])}}" label="{{$tag->name}}"
+                         sizeLabel="{{$buttonSizeLabel}}"
+                         withLabel="{{$buttonWithLabel}}"
+                         color="{{$buttonColor}}"
+                         style="{{$buttonStyle}}"
+                         onclick="{{$buttonOnclick}}"
+                         withIcon="{{$buttonWithIcon}}"
+                         iconClass="{{$buttonIconClass}}"
+                         target="{{$buttonTarget}}"
+                         iconPosition="{{$buttonIconPosition}}"
+                         iconColor="{{$buttonIconColor}}"
+        />
       @endforeach
     </span>
   @endif
