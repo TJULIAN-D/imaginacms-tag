245473d52b1abbd3e4ab9a96676dd87f4ed639a5
Cristian Marin <marin101998@gmail.com>
2024-05-21 15:28:02 -0500

    +fix | v10.x 21-05-24 add function get items by in cache decorator

--- Repositories/Cache/CacheTagDecorator.php
+++ Repositories/Cache/CacheTagDecorator.php
@@ -8,20 +8,27 @@ use Modules\Tag\Repositories\TagRepository;
 
 class CacheTagDecorator extends BaseCacheDecorator implements TagRepository
 {
-    public function __construct(TagRepository $tag)
-    {
-        parent::__construct();
-        $this->entityName = 'tag.tags';
-        $this->repository = $tag;
-    }
+  public function __construct(TagRepository $tag)
+  {
+    parent::__construct();
+    $this->entityName = 'tag.tags';
+    $this->repository = $tag;
+  }
 
-    /**
-     * Get all the tags in the given namespace
-     */
-    public function allForNamespace($namespace)
-    {
-        return $this->remember(function () use ($namespace) {
-            return $this->repository->allForNamespace($namespace);
-        });
-    }
+  /**
+   * Get all the tags in the given namespace
+   */
+  public function allForNamespace($namespace)
+  {
+    return $this->remember(function () use ($namespace) {
+      return $this->repository->allForNamespace($namespace);
+    });
+  }
+
+  public function getItemsBy($params)
+  {
+    return $this->remember(function () use ($params) {
+      return $this->repository->getItemsBy($params);
+    });
+  }
 }
